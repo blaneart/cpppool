7b57f2c07e55a6df35009df5d4d7d4a06f5f55d2
Artem BLANAR <ablanar@student.42.fr>
2020-09-14 20:47:09 +0200

    ref

--- day01/ex06/HumanA.cpp
+++ day01/ex06/HumanA.cpp
@@ -1,8 +1,8 @@
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon) : weapon(&weapon), name(name)
+HumanA::HumanA(std::string name, Weapon &weapon) : weapon(weapon), name(name)
 {}
 void HumanA::attack()
 {
-	std::cout << name << " attacks with his " << weapon->getType() << std::endl;
+	std::cout << name << " attacks with his " << weapon.getType() << std::endl;
 }
