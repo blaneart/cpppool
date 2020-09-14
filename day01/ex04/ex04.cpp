1db4790c5bda1fcb7789c5d96e5b8c0b9c67706a
Artem BLANAR <ablanar@student.42.fr>
2020-09-14 20:46:16 +0200

    ref

--- day01/ex04/ex04.cpp
+++ day01/ex04/ex04.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	std::string str = "HI THIS IS BRAIN";
 	std::string *ptr = &str;
-	std::string ref = *ptr;
+	std::string &ref = str;
 
 	std::cout << *ptr << std::endl;
 	std::cout << ref << std::endl;
