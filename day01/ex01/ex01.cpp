6b6a289d30342c32137eecbebeddbba0fce47434
Artem BLANAR <ablanar@student.42.fr>
2020-09-14 20:14:00 +0200

    main

--- day01/ex01/ex01.cpp
+++ day01/ex01/ex01.cpp
@@ -1,6 +1,27 @@
+#include "iostream"
+#include "string"
+
 void memoryLeak()
 {
 	std::string*        panther = new std::string("String panther");
 	std::cout << *panther << std::endl;
+}
+
+void resolvedmemoryLeak1()
+{
+	std::string*        panther = new std::string("String panther");
+	std::cout << *panther << std::endl;
 	delete panther;
+}
+
+void resolvedmemoryLeak2()
+{
+	std::string panther("String panther");
+	std::cout << panther << std::endl;
+}
+
+int main()
+{
+	resolvedmemoryLeak2();
+	return (0);
 }
