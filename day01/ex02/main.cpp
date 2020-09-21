19d8ad38e2543585d3bf3622d447c38413257a71
Artem BLANAR <ablanar@student.42.fr>
2020-09-21 15:47:24 +0200

    example added

--- day01/ex02/main.cpp
+++ day01/ex02/main.cpp
@@ -6,7 +6,7 @@ int main()
 	Zombie *exmpl;
 	lol.setZombieType("ded");
 	lol.randomChump();
-	exmpl = lol.newZombie();
+	exmpl = lol.newZombie("Tank");
 	exmpl->announce();
 	delete exmpl;
 	return 0;
