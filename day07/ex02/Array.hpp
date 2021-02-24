c2ca14e8b8387bf470c2b6618dda12d6aedf510f
Artem BLANAR <ablanar@student.42.fr>
2021-02-24 15:11:42 +0100

    correct ex02

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -1,6 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablanar <ablanar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 14:46:30 by ablanar           #+#    #+#             */
+/*   Updated: 2021/02/24 15:00:24 by ablanar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 #include <iostream>
+#include <exception>
 
 template <typename T>
 class Array
@@ -20,5 +34,74 @@ class Array
 		};
 		unsigned int size( void ) const;
 };
+
+
+template<typename T>
+Array<T>::Array()
+{
+	this->array = new T[0];
+	this->length = 0;
+}
+
+template<typename T>
+Array<T>::Array(unsigned int n)
+{
+	this->array = new T[n];
+	this->length = n;
+	for (unsigned int i = 0; i < n; i++)
+	{
+		// This initializes every cell to type's default value;
+		this->array[i] = T();
+	}
+}
+
+template<typename T>
+Array<T>::Array(const Array& other)
+{
+	this->length = other.size();
+	this->array = new T[this->length];
+	for (unsigned int i = 0; i < this->length; i++)
+		this->array[i] = other.array[i];
+}
+
+template<typename T>
+Array<T>& Array<T>::operator=(const Array& other)
+{
+	this->length = other.size();
+	this->array = new T[this->length];
+	for (unsigned int i = 0; i < this->length; i++)
+		this->array[i] = other.array[i];
+	return *this;
+}
+
+template<typename T>
+const char* Array<T>::OutOfLimitsException::what() const throw()
+{
+	return "ArrayException: index out of limits";
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	if (this->array)
+		delete this->array;
+	this->array = NULL;
+	this->length = 0;
+}
+
+template<typename T>
+T& Array<T>::operator[](unsigned int i)
+{
+	if (this->length <= i)
+		throw Array::OutOfLimitsException();
+	return this->array[i];
+
+}
+
+template<typename T>
+unsigned int Array<T>::size() const
+{
+	return this->length;
+}
 
 #endif
